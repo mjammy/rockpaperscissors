dac8cc4a6021f0ea4191ecf40f2d978c5d7e3660
musa jamshed <Musa@musas-air.lan>
2020-04-05 22:59:24 -0400

    Front end is communicating with api wooooo

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -9,7 +9,30 @@ class App extends React.Component {
     super(props);
     this.state = {
       scores: [0,0,0],
+      sponsor: null,
     };
+  }
+
+  ////////////////////////////////////////////////////
+
+  componentDidMount() {
+    fetch("/sponsor")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            sponsor: result.sponsor
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
   }
 
   ////////////////////////////////////////////////////
@@ -31,6 +54,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>This App is Sponsored by {this.state.sponsor}</h1>
+        <hr />
         <Scoreboard scores={this.state.scores}
                     onReset={() => this.handleReset()}/>
         <Game scores={this.state.scores} 
